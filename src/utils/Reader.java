b0243eb61d7173949b3b15ccfc8c62807ec91ba7
Paulo Lopes <paulo.hq.lopes@gmail.com>
2018-03-24 10:47:01 -0300

    Working on Reader

--- src/utils/Reader.java
+++ src/utils/Reader.java
@@ -1,5 +1,42 @@
 package utils;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Reader {
+
+	private String path;
+	private ArrayList<ArrayList<Integer>> matrix;
+
+	public Reader(String fileName) {
+		this.path = fileName;
+	}
+
+	public  ArrayList<ArrayList<Integer>> readFile() throws FileNotFoundException, IOException {
+		try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+			reader.readLine(); // linha com parametros
+
+			for (String matrixLine; (matrixLine = reader.readLine()) != null;) {
+				int[] array = Arrays.asList(matrixLine.split(" ")).stream().mapToInt(Integer::parseInt).toArray();
+				List<Integer> list = Arrays.stream(array).boxed().collect(Collectors.toList());
+				ArrayList<Integer> line = new ArrayList<Integer>(list);
+				matrix.add(line);
+			}
+		}
+		return getMatrix();
+	}
+
+	private  ArrayList<ArrayList<Integer>> getMatrix() {
+		return matrix;
+	}
+
+	
+	
 
 }
